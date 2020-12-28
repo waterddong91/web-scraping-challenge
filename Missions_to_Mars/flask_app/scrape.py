27fb8abb54804c5a063093dccdafdf7e261e532d
DongYi Kim <waterddong91@gmail.com>
2020-12-27 23:17:29 -0500

    mission_to_mars_scripts

---
+++ Missions_to_Mars/flask_app/scrape.py
@@ -0,0 +1,64 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Collecting splinter\n",
+      "  Downloading splinter-0.14.0-py3-none-any.whl (36 kB)\n",
+      "Requirement already satisfied: six in c:\\users\\dongyi\\anaconda3\\lib\\site-packages (from splinter) (1.15.0)\n",
+      "Collecting selenium>=3.141.0\n",
+      "  Downloading selenium-3.141.0-py2.py3-none-any.whl (904 kB)\n",
+      "Requirement already satisfied: urllib3 in c:\\users\\dongyi\\anaconda3\\lib\\site-packages (from selenium>=3.141.0->splinter) (1.25.9)\n",
+      "Installing collected packages: selenium, splinter\n",
+      "Successfully installed selenium-3.141.0 splinter-0.14.0\n",
+      "Note: you may need to restart the kernel to use updated packages.\n"
+     ]
+    }
+   ],
+   "source": [
+    "pip install splinter"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
